7eef1779fc93fb0e142bdeb46f68e0cde1da3c55
ChingHsuan Tsai <a0926985717@gmail.com>
2024-10-24 04:03:07 +0100

    create products.js

---
+++ products.js
@@ -0,0 +1,48 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Dance",
+        "location": "London",
+        "price": 20.00,
+        "image": "image/dance.jpg",
+        "space": 10,
+        "rating": 2
+    },
+   { 
+        "id": 1002,
+        "title": "Cooking",
+        "location": "Manchester",
+        "price": 20.00,
+        "image": "image/cooking.jpg",
+        "space": 10,
+        "rating": 5
+   },
+   { 
+        "id": 1003,
+        "title": "Art",
+        "location": "Birmingham",
+        "price": 20.00,
+        "image": "image/art.jpg",
+        "space": 10,
+        "rating": 3
+    },
+   {
+         "id": 1004,
+         "title": "Math",
+        "location": "Liverpool",
+        "price": 20.00,
+        "image": "image/math.jpg",
+        "space": 10,
+        "rating": 4
+    },
+   { 
+        "id": 1005,
+        "title": "Photography",
+        "location": "Edinburgh",
+        "price": 20.00,
+        "image": "image/Photography.jpg",
+        "space": 10,
+        "rating": 5
+    }
+   ]
+   
