0d6ade9095d4bc25cee29f5ad776fbe221ec78a5
ChingHsuan Tsai <a0926985717@gmail.com>
2024-10-25 20:34:30 +0100

    add more lessons

--- products.js
+++ products.js
@@ -12,7 +12,7 @@ let products = [
         "id": 1002,
         "title": "Cooking",
         "location": "Manchester",
-        "price": 20.00,
+        "price": 25.00,
         "image": "image/cooking.jpg",
         "space": 10,
         "rating": 5
@@ -21,7 +21,7 @@ let products = [
         "id": 1003,
         "title": "Art",
         "location": "Birmingham",
-        "price": 20.00,
+        "price": 30.00,
         "image": "image/art.jpg",
         "space": 10,
         "rating": 3
@@ -39,10 +39,55 @@ let products = [
         "id": 1005,
         "title": "Photography",
         "location": "Edinburgh",
-        "price": 20.00,
+        "price": 15.00,
         "image": "image/Photography.jpg",
         "space": 10,
         "rating": 5
+    },
+    { 
+        "id": 1006,
+        "title": "Swimming",
+        "location": "Leeds",
+        "price": 15.00,
+        "image": "image/swim.jpg",
+        "space": 10,
+        "rating": 4
+    },
+    { 
+        "id": 1007,
+        "title": "Music",
+        "location": "Oxford",
+        "price": 25.00,
+        "image": "image/music.jpg",
+        "space": 10,
+        "rating": 5
+    },
+    { 
+        "id": 1008,
+        "title": "Writing",
+        "location": "Bath",
+        "price": 25.00,
+        "image": "image/writing.jpg",
+        "space": 10,
+        "rating": 2
+    },
+    { 
+        "id": 1009,
+        "title": "Programming",
+        "location": "York",
+        "price": 35.00,
+        "image": "image/Programming.jpg",
+        "space": 10,
+        "rating": 3
+    },
+    { 
+        "id": 1010,
+        "title": "Biology",
+        "location": "Sheffield",
+        "price": 15.00,
+        "image": "image/biology.jpg",
+        "space": 10,
+        "rating": 4
     }
    ]
    
